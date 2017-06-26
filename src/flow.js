355cddc8692f345329bc8708d18c9320e27a8f79
Pavel Rumkin <dev@rumk.in>
2017-06-26 20:36:12 +0300

    [src] update flow add whileNot method

--- src/flow.js
+++ src/flow.js
@@ -43,27 +43,39 @@ function stack(...handlers) {
 }
 
 /**
+ * Create async queue resolver which works while condition returns false.
+ * @param  {function(object)} contition Condition function which returns bool.
+ * @return {function(...function()|Handlable)} Handlable async queue handler creator.
+ */
+function whileNot(contition) {
+  return function(...args) {
+    const handlers = args.map(getHandler);
+
+    return async function(ctx, next) {
+      for (const handler of handlers) {
+        await handler(Object.assign({}, ctx), () => {});
+
+        if (contition(ctx) === true) {
+          return;
+        }
+      }
+
+      await next();
+    };
+  };
+}
+
+/**
  * Create async request handlers queue. It iterate request handlers and if
  * request handler doesn't sent response it runs next request handler and so.
  * @param  {...function()|Handlable} handlers Handlable async functions.
  * @return {function(object,function)} Returns function which pass context through the queue.
  */
-function or(...args) {
-  const handlers = args.map(getHandler);
-
-  return async function(ctx, next) {
-    for (const handler of handlers) {
-      await handler(Object.assign({}, ctx), () => {});
-
-      if (ctx.res.headersSent) {
-        return;
-      }
-    }
-
-    await next();
-  };
-}
+const or = whileNot(function({res}) {
+  return res.headersSent;
+});
 
 exports.stack = stack;
 exports.or = or;
+exports.whileNot = whileNot;
 exports.getHandler = getHandler;
