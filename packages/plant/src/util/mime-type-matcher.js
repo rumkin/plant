52ac2e980bf089f2b14dc6f6b42a6905f7414806
Pasha Rumkin <dev@rumk.in>
2018-12-15 19:25:11 +0300

    [src,docs] Add jsdoc comments

--- packages/plant/src/util/mime-type-matcher.js
+++ packages/plant/src/util/mime-type-matcher.js
@@ -1,5 +1,12 @@
 const escapeRegExp = require('lodash.escaperegexp');
 
+/**
+ * getMimeMatcher - create mime type matcher function wich determines weather
+ * passed `type` matches `types`,
+ *
+ * @param  {Array<String|RegExp>} types List of types.
+ * @return {function(String):Boolean} Type matcher function.
+ */
 function getMimeMatcher(types) {
   const matchers = types.map((type) => {
     if (typeof type === 'string') {
@@ -23,9 +30,17 @@ function getMimeMatcher(types) {
   };
 }
 
+/**
+ * stringMatcher - Return matcher function. If `origin` contains '*' it will
+ * convert string to regexp and call regExpMatcher. Other way it will return
+ * a function which check strings equality.
+ *
+ * @param  {String} origin Mime type or mime type mask.
+ * @return {function(String):Boolean} returns matcher function.
+ */
 function stringMatcher(origin) {
   if (origin.includes('*')) {
-    return regExpMatcher(toRegExp(origin));
+    return regExpMatcher(toRegExp(origin, '[^\/]+'));
   }
 
   return function(value) {
@@ -33,14 +48,28 @@ function stringMatcher(origin) {
   };
 }
 
+/**
+ * regExpMatcher - return regexp matcher function.
+ *
+ * @param  {RegExp} regexp Regular exression to match with.
+ * @return {function(String):Boolean} Returns matcher function.
+ */
 function regExpMatcher(regexp) {
   return function(value) {
     return regexp.test(value);
   };
 }
 
-function toRegExp(mask) {
-  const re = mask.split('*').map(escapeRegExp).join('[^\/]+');
+/**
+ * toRegExp - Convert text `mask` to regular expression. Asterisk will be replaced
+ * with `replacer`.
+ *
+ * @param  {String} mask String containing asterisk.
+ * @param  {String} replacer Regular expression to substitute of asterisk.
+ * @return {RegExp}      Regular expression.
+ */
+function toRegExp(mask, replacer = '.+?') {
+  const re = mask.split('*').map(escapeRegExp).join(replacer);
 
   return new RegExp('^' + re + '$');
 }
