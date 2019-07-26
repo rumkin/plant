eba9b6dae593af1b81532242478c711bc6045a5d
Pavel Rumkin <dev@rumk.in>
2019-07-26 12:14:21 +0300

    [util] split lint aand test commands code. Make full test on push only

--- test.sh
+++ test.sh
@@ -10,16 +10,56 @@ run_test() {
   echo "TEST: $DIR"
 
   cd "$CWD/packages/$DIR"
+
   [ "$INSTALL" == "1" ] && npm i .
-  [ "$LINT" == "1" ] && npm run lint
   npm test
 }
 
-run_test plant
-run_test http-adapter
-run_test http
-run_test https
-run_test http2
-run_test https2
-run_test router
-run_test flow
+run_lint() {
+  local DIR=$1
+
+  echo "LINT: $DIR"
+
+  cd "$CWD/packages/$DIR"
+
+  npm run lint
+}
+
+cmd_test() {
+  run_test plant
+  run_test http-adapter
+  run_test http
+  run_test https
+  run_test http2
+  run_test https2
+  run_test router
+  run_test flow
+}
+
+cmd_lint() {
+  run_lint plant
+  run_lint http-adapter
+  run_lint http
+  run_lint https
+  run_lint http2
+  run_lint https2
+  run_lint router
+  run_lint flow
+}
+
+cmd_usage() {
+  echo "Usage is: test.sh COMMAND"
+  echo ""
+  echo "Commands are:"
+  echo "- test"
+  echo "- lint"
+}
+
+CMD=$1
+shift 1
+
+case $CMD in
+  "lint") cmd_lint $@ ;;
+  "test") cmd_test $@ ;;
+  *) cmd_usage ;;
+esac
