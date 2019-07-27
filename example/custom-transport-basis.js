56884eaed4f7712ef965e61c7079406f27e8c1c0
Pavel Rumkin <dev@rumk.in>
2019-07-27 15:36:47 +0300

    [docs:lint] Fix custom transport code

--- example/custom-transport-basis.js
+++ example/custom-transport-basis.js
@@ -1,3 +1,5 @@
+const Plant = require('@plant/plant')
+
 const plant = new Plant()
 
 const url = new URL('http://localhost:8080/')
@@ -5,10 +7,10 @@ const url = new URL('http://localhost:8080/')
 // Create HTTP context's params
 const req = new Plant.Request({
   url,
-});
+})
 const res = new Plant.Response({
   url,
-});
+})
 
 // Request peer. Peer represents other side of connection.
 const peer = new Plant.Peer({
@@ -16,22 +18,20 @@ const peer = new Plant.Peer({
     protocol: 'proc:',
     hostname: process.pid,
   }),
-});
+})
 
 // Create connection socket
 const socket = new Plant.Socket({
   peer,
   // If socket allows write upstream, then onPush method could be defined to handle pushes.
   // onPush should return Promise which resolves when response sending completes.
-  async onPush(response) {},
-});
+  // eslint-disable-next-line no-unused-vars
+  async onPush(response) {
+    // Send response with web socket
+  },
+})
 
 const handleRequest = plant.getHandler()
 
 handleRequest({req, res, socket})
-.then(() => {
-  // Request handled. All requests (even faulty) should get there.
-}, (error) => {
-  // Something went wrong
-})
-.then(() => socket.destroy())
+.finally(() => socket.destroy())
