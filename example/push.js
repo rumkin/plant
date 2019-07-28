731e473740f6361c2269eeb9107b4d6f79bcc080
Pavel Rumkin <dev@rumk.in>
2019-07-28 18:55:21 +0300

    [docs] Add http push example

---
+++ example/push.js
@@ -0,0 +1,17 @@
+import Plant from '@plant/plant'
+import {createServer} from '@plant/http2'
+import {serveDir} from '@plant/fs'
+
+const plant = new Plant()
+
+plant.use('/assets', serveDir('./public'))
+
+plant.use(({res}) => {
+  res.push('/assets/index.js')
+  res.push('/assets/style.css')
+
+  res.html('<!DOCTYPE html><html><head>...')
+})
+
+createServer(plant)
+.listen(8080)
